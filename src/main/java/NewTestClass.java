d0f6c6fc0ae5fc0e42c0115b31cb82bfcacf6ca7
hillelsummer2021 <hillel.summer2021@i.ua>
2021-07-28 20:40:09 +0300

    added new Google Search test

--- src/main/java/NewTestClass.java
+++ src/main/java/NewTestClass.java
@@ -4,6 +4,7 @@ public class NewTestClass {
     public void newMethod(){
         System.out.println("feowhf");
         System.out.println("Something");
+        System.out.println("Google Search");
     }
 
 
