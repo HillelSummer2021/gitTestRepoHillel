1eecc1c95dea365141241971c182cd2fa7f53edd
HillelSummer2021 <87911060+HillelSummer2021@users.noreply.github.com>
2021-07-28 20:59:54 +0300

    added new Google Search test (#1)
    added new Google Search test

--- src/main/java/NewTestClass.java
+++ src/main/java/NewTestClass.java
@@ -4,6 +4,9 @@ public class NewTestClass {
     public void newMethod(){
         System.out.println("feowhf");
         System.out.println("Something");
+        System.out.println("Google Search");
+//        Thread.sleep();
+        System.out.println("Code without thread sleep");
 
         System.out.println("Thread sleep removed");
         System.out.println("some new code that were merged");
