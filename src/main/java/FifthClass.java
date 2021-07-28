aaaf69951dacf2d5d0df69dfd48e65e5fb913097
sergiy.belevitnev <sergiy.belevitnev@shutterfly.com>
2021-07-28 16:43:26 +0300

    Revert "28 july commit"
    This reverts commit 1eea99fc

--- src/main/java/FifthClass.java
+++ src/main/java/FifthClass.java
@@ -1,11 +1,2 @@
 public class FifthClass {
-
-    public void printSomething(){
-
-        System.out.println("");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-
-    }
 }
