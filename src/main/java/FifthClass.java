1eea99fc97b33a2c606ccc09dace793459f36e49
sergiy.belevitnev <sergiy.belevitnev@shutterfly.com>
2021-07-28 16:41:38 +0300

    28 july commit

--- src/main/java/FifthClass.java
+++ src/main/java/FifthClass.java
@@ -1,2 +1,11 @@
 public class FifthClass {
+
+    public void printSomething(){
+
+        System.out.println("");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+
+    }
 }
