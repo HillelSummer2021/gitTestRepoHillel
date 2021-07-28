ef535808712bddda2eec3701939e2b898adde9a9
hillelsummer2021 <hillel.summer2021@i.ua>
2021-07-28 20:50:51 +0300

    master branch update

--- src/main/java/NewTestClass.java
+++ src/main/java/NewTestClass.java
@@ -4,6 +4,10 @@ public class NewTestClass {
     public void newMethod(){
         System.out.println("feowhf");
         System.out.println("Something");
+
+        System.out.println("Thread sleep removed");
+        System.out.println("some new code that were merged");
+
     }
 
 
